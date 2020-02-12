2a615baf876df9c987e810c875f4e1380083d0f0
rafiudd <sirin6867@gmail.com>
2020-02-12 22:43:52 +0700

    fix fullname undifined

--- modules/quiz/quiz.controller.js
+++ modules/quiz/quiz.controller.js
@@ -139,7 +139,6 @@ async function checkAnswer(req,res) {
     let resultAnswer = {
         "quizId" : model._id,
         "userId" : userId,
-        "fullname" : query.fullname,
         "email" : query.email,
         "userAnswer" : payloadAnswer,
         "trueAnswer" : isTrue,
